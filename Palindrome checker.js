2539accf29951b8159cf3298e95e0bafc7f9a2b0
Godwin Bassey <godwinobibassey@gmail.com>
2023-09-08 15:52:22 +0100

    I made a palindrome checker

---
+++ Palindrome checker.js
@@ -0,0 +1,17 @@
+function palindrome(str) {
+    let removeSpecial = str.replace(/[^a-zA-Z0-9]/gi, '')
+    removeSpecial = removeSpecial.toLowerCase()
+    let reverseStr = ''
+    for(let i = removeSpecial.length - 1; i >=0 ; i--){
+      reverseStr += removeSpecial[i]
+    }
+  
+    if (reverseStr === removeSpecial){
+      console.log(`${reverseStr} and ${removeSpecial} are palindromes`)  
+      return true
+    } else {
+        console.log(`${reverseStr} and ${removeSpecial} are NOT palindromes`)
+        return false
+    }
+  }
+palindrome('eye')
--- README.md
+++
@@ -1,3 +0,0 @@
-# FCCJavaScriptProjects
-
-This includes all the projects for my JS certification on FreeCodeCamp's Javascript Data Structures and Algorithm's course
